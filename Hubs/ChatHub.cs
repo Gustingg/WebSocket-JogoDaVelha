84bc04b6ec1466836e6afa6f4a42427ed0000bd6
Gustavo Cordeiro <gustavocordeiro@geodados.com>
2021-06-01 16:46:23 -0300

    Adicionar arquivos de projeto.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSocket.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", Context.ConnectionId, message);
+        }
+
+        public async Task SetPlayer1()
+        {
+            await Clients.All.SendAsync("SetPlayer1", Context.ConnectionId);
+        }
+        public async Task SetPlayer2()
+        {
+            await Clients.All.SendAsync("SetPlayer2", Context.ConnectionId);
+        }
+
+        public async Task SqrGame(string sqr)
+        {
+            await Clients.All.SendAsync("SqrGame", sqr, Context.ConnectionId);
+        }
+        public async Task ResetGame()
+        {
+            await Clients.All.SendAsync("ResetGame");
+        }
+    }
+}
